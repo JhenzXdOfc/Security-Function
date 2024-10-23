e5be4f1e343198c66afbe56585c009e453815ab9
JhenzXdOfc <102642866+JhenzXdOfc@users.noreply.github.com>
2024-10-23 15:27:59 +0700

    Add files via upload

---
+++ adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    approval: {
+        num: "62888888",
+        text: "setujui",
+        set: "Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+        greet: "Akses Di Setujui ✅"
+    },
+    creatorScript: '628888888',
+    filePath: './approval',
+    checkFilePath: './case.js',
+    codeToDetect: `KyuuStart();`
+};
